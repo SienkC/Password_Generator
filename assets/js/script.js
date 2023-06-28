f7c4670e8fddc1755046fc110bf8ed54c74fb353
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-06-28 10:55:31 -0400

    Updated function to return string and moved function declarations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  return;
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 function generatePassword(){
   // Temp value
   var randPass = "Hello";
 
+  // test
   console.log(randPass);
 
-  
-  return ;
-  // randPass;
+  // Sends random password
+  return randPass;
 }
